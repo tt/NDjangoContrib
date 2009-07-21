a684188d448fd3231016e3ca978fdba9de47c52e
Troels Thomsen <t.thomsen@judoo.dk>
2009-07-21 09:17:32 +0200

    Implemented Associated Press number formatting filter.

--- src/NDjango.Contrib/Filters/Humanize/ApNumber.cs
+++ src/NDjango.Contrib/Filters/Humanize/ApNumber.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Microsoft.FSharp.Core;
 
 namespace NDjango.Contrib.Filters.Humanize
 {
@@ -8,7 +8,45 @@ namespace NDjango.Contrib.Filters.Humanize
 
 		public object Perform(object __p1)
 		{
-			throw new NotImplementedException();
+			Tuple<bool, int> tuple = Utilities.get_int(__p1);
+
+			if (!tuple.Item1)
+			{
+				return string.Empty;
+			}
+
+			// TODO: Extract these strings into resource files to allow localization
+			switch (tuple.Item2)
+			{
+				case 1:
+					return "one";
+
+				case 2:
+					return "two";
+
+				case 3:
+					return "three";
+
+				case 4:
+					return "four";
+
+				case 5:
+					return "five";
+
+				case 6:
+					return "six";
+
+				case 7:
+					return "seven";
+
+				case 8:
+					return "eight";
+
+				case 9:
+					return "nine";
+			}
+
+			return tuple.Item2;
 		}
 
 		#endregion
