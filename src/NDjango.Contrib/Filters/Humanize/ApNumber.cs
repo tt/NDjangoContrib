da7ffb4c18ddb70f5e4459ed6e8156e3e3160540
Troels Thomsen <t.thomsen@judoo.dk>
2009-07-21 09:02:52 +0200

    Added stub class for Associated Press style number formatting.

---
+++ src/NDjango.Contrib/Filters/Humanize/ApNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NDjango.Contrib.Filters.Humanize
+{
+	public class ApNumber : Interfaces.ISimpleFilter
+	{
+		#region ISimpleFilter Members
+
+		public object Perform(object __p1)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
