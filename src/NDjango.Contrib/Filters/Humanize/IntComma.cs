56d56062810fcf7f04e455921d1fef86c411f964
Troels Thomsen <t.thomsen@judoo.dk>
2009-07-21 09:24:11 +0200

    Implemented filter and added link to filter reference on the Django documentation.

--- src/NDjango.Contrib/Filters/Humanize/IntComma.cs
+++ src/NDjango.Contrib/Filters/Humanize/IntComma.cs
@@ -1,14 +1,24 @@
-﻿using System;
+﻿using Microsoft.FSharp.Core;
 
 namespace NDjango.Contrib.Filters.Humanize
 {
+	/// <remarks>
+	/// http://docs.djangoproject.com/en/dev/ref/contrib/humanize/#intcomma
+	/// </remarks>
 	public class IntComma : Interfaces.ISimpleFilter
 	{
 		#region ISimpleFilter Members
 
 		public object Perform(object __p1)
 		{
-			throw new NotImplementedException();
+			Tuple<bool, int> tuple = Utilities.get_int(__p1);
+
+			if (!tuple.Item1)
+			{
+				return string.Empty;
+			}
+
+			return tuple.Item2.ToString("#,0");
 		}
 
 		#endregion
