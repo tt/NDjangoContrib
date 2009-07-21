a990f59f8b07e9c8f48de61eea75ca247efe62ab
Troels Thomsen <t.thomsen@judoo.dk>
2009-07-21 09:38:05 +0200

    Added stub classes for remaining filters in "django.contrib.humanize".

---
+++ src/NDjango.Contrib/Filters/Humanize/IntWordFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NDjango.Contrib.Filters.Humanize
+{
+	public class IntWordFilter : Interfaces.ISimpleFilter
+	{
+		#region ISimpleFilter Members
+
+		public object Perform(object __p1)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
---
+++ src/NDjango.Contrib/Filters/Humanize/NaturalDayFilter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NDjango.Contrib.Filters.Humanize
+{
+	public class NaturalDayFilter : Interfaces.IFilter
+	{
+		#region IFilter Members
+
+		public object DefaultValue
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public object PerformWithParam(object __p1, object __p2)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+		#region ISimpleFilter Members
+
+		public object Perform(object __p1)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
